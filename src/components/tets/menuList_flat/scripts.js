596a884aa28208589044a12a56e6802a2ca8bc79
Sergey Sosnovskiy <serega0005@gmail.com>
2018-03-02 11:46:44 +0700

    Компоненты меню

--- src/components/tets/menuList_flat/scripts.js
+++
@@ -1,9 +0,0 @@
-var $ = require('jquery')
-
-$('body').on('click', '.js-menuList .menuList_toggler', function (e) {
-  $(this).parents('.menuList__item').eq(0).toggleClass('isOpen')
-})
-
-$('body').on('click', '.js-menuList .isDisabled', function (e) {
-  e.preventDefault()
-})
