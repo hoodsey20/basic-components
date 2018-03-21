c89a698b11c9dbee7a0b92022ab134ef95538c93
Артём <hoodsey20@gmail.com>
2018-03-21 15:57:12 +0700

    убрал предыдущее решение компонента input, добавил input, input_brdr

---
+++ src/components/input_brdr/scripts.js
@@ -0,0 +1,9 @@
+var $ = require('jquery');
+
+$('body').on('change', '.js_inputFill', function (e) {
+  if ($(this).val()) {
+    $(this).siblings('.form__inputName').addClass('isFill');
+  } else {
+    $(this).siblings('.form__inputName').removeClass('isFill');
+  }
+})
