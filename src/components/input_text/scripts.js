48e7983e2445f74d1d5b4eb3dbfe87091b325306
Артём <hoodsey20@gmail.com>
2018-03-21 11:11:43 +0700

    input без модификаторов

--- src/components/input_text/scripts.js
+++
@@ -1,9 +0,0 @@
-var $ = require('jquery')
-
-$('body').on('change', '.js_inputFill', function (e) {
-  if ($(this).val()) {
-    $(this).siblings('.form__inputName').addClass('isFill')
-  } else {
-    $(this).siblings('.form__inputName').removeClass('isFill')
-  }
-})
