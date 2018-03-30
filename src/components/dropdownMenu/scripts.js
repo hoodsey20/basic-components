3e087e1c4790c0bb2180437c46c9e18c4addc353
Артём <hoodsey20@gmail.com>
2018-03-30 15:41:12 +0700

    dropdownMenu

--- src/components/dropdownMenu/scripts.js
+++ src/components/dropdownMenu/scripts.js
@@ -1,28 +1,43 @@
 var $ = require('jquery')
 
 $('.js-dropdownMenu').each(function (index, item) {
-  var currentText = $(item).find('.dropdownMenu__item.isActive').text()
-  $(item).find('.dropdownMenu__currentValue').html(currentText)
-})
+  var activeItem = $(item).find('.dropdownMenu__item.isActive');
+  var input = $(item).find('.dropdownMenu__input');
+  var currentValueContainer = $(item).find('.dropdownMenu__currentValue');
+
+  var currentText = activeItem.text();
+  var currentDataValue = activeItem.data('value');
+
+  input.attr('value', currentDataValue);
+
+  if (activeItem.length) {
+    currentValueContainer.html(currentText);
+  }
+});
 
 $('body').on('click', '.js-dropdownMenu', function (e) {
-  var dropDownList = $(this).find('.dropdownMenu__list')
+  var dropDownList = $(this).find('.dropdownMenu__list');
   if (!dropDownList.is(e.target) && dropDownList.has(e.target).length === 0) {
-    $(this).toggleClass('isOpen')
+    $(this).toggleClass('isOpen');
   }
 })
 
 $(document).mouseup(function (e) {
   if (!$('.js-dropdownMenu').is(e.target) && $('.js-dropdownMenu').has(e.target).length === 0) {
-    $('.js-dropdownMenu').removeClass('isOpen')
+    $('.js-dropdownMenu').removeClass('isOpen');
   }
 })
 
 $('body').on('click', '.js-dropdownMenu .dropdownMenu__item', function (e) {
-  var dropDown = $(this).parents('.js-dropdownMenu')
-  dropDown.toggleClass('isOpen')
-  var currentText = $(this).text()
-  dropDown.find('.dropdownMenu__currentValue').html(currentText)
-  dropDown.find('.dropdownMenu__item').removeClass('isActive')
-  $(this).addClass('isActive')
+  var dropDown = $(this).parents('.js-dropdownMenu');
+  var currentText = $(this).text();
+  var input = dropDown.find('.dropdownMenu__input');
+  var currentDataValue = $(this).data('value');
+
+  input.attr('value', currentDataValue);
+  dropDown.find('.dropdownMenu__currentValue').html(currentText);
+  dropDown.find('.dropdownMenu__item').removeClass('isActive');
+
+  dropDown.toggleClass('isOpen');
+  $(this).addClass('isActive');
 })
