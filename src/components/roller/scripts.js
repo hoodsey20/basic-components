4cce66ba90535e6c70e13f28cd190603bdf6d973
Артём <hoodsey20@gmail.com>
2018-04-01 14:15:58 +0700

    рефакторинг переменных компонента roller

--- src/components/roller/scripts.js
+++ src/components/roller/scripts.js
@@ -3,50 +3,69 @@
 window.roller = function (params) {
   if (!params.parentElement) return false;
 
-  var rollerParent = params.parentElement;
-  var min = params.minValue;
-  var max = params.maxValue;
-  var roller = rollerParent.querySelectorAll('.roller__circle')[0];
-  var shaftIndicator = rollerParent.querySelector('.roller__shaft-indicator');
+  // consts
+  var MIN = params.minValue;
+  var MAX = params.maxValue;
+  var STEP = params.step || (MAX - MIN) / 20;
+  var DIFFERENCE = MAX - MIN;
+
+  // nodes
+  var rollerNode = params.parentElement;
+  var rollerHandler = rollerNode.querySelectorAll('.roller__circle')[0];
+  var rollerHandler2 = null;
+  var shaftIndicator = rollerNode.querySelector('.roller__shaft-indicator');
+  var isSecondHandler = false;
+  if (params.secondRoller && rollerNode.querySelectorAll('.roller__circle').length > 1) {
+    rollerHandler2 = rollerNode.querySelectorAll('.roller__circle')[1];
+    isSecondHandler = true;
+  }
+
+  // callback vars
+  var value1 = MIN;
+  var value2 = MAX;
   var currentChangedInput = false;
-  var roller2Exist = false;
-  var currentTarget = roller;
-  var roller1position = 0;
-  var roller2position = 0;
-  var value1 = min;
-  var value2 = max;
-  var step = params.step || (max - min) / 20;
-  // разница между максимальным и минимальным значением на оси X
-  var differenceValue = max - min;
+
+  var currentTarget = rollerHandler;
+  var handlerPosition = 0;
+  var secondHandlerPosition = 0;
+
+  var maxRight = rollerNode.offsetWidth;
+  var parentLongitudeFromLeft = rollerNode.getBoundingClientRect().left;
+  console.log(maxRight, parentLongitudeFromLeft);
   var shaftIndicatorFromLeft = 0;
   var shaftIndicatorFromRight = 0;
 
-  var rollerWidth = rollerParent.offsetWidth;
-  var circleWidth = roller.offsetWidth;
-  var circleToRollerRatio = circleWidth / rollerWidth;
-  var defaultGap = Math.floor(differenceValue * circleToRollerRatio) * 2;
-  var gap = params.gap || defaultGap;
-  var procentGap = Math.floor((gap * 100) / (max - min));
-  var maxRight = rollerParent.offsetWidth;
-  var parentLongitude = rollerParent.getBoundingClientRect().left;
+  var getProcentGap = function () {
+    var procentGap = null;
 
-  if (!!params.secondRoller && rollerParent.querySelectorAll('.roller__circle').length > 1) {
-    var roller2 = rollerParent.querySelectorAll('.roller__circle')[1];
-    roller2Exist = true;
+    if (params.gap) {
+      procentGap = Math.floor((params.gap * 100) / (MAX - MIN));
+      return procentGap;
+    }
+
+    var rollerWidth = rollerNode.offsetWidth;
+    var circleWidth = rollerHandler.offsetWidth;
+    var circleToRollerRatio = circleWidth / rollerWidth;
+    var defaultGap = Math.floor(DIFFERENCE * circleToRollerRatio) * 2;
+    var gap = defaultGap;
+
+    procentGap = Math.floor((gap * 100) / (MAX - MIN));
+
+    return procentGap;
   }
 
   var roundOff = function (val) {
     var value = Number(val);
 
-    if (value === step) return value;
+    if (value === STEP) return value;
 
-    var numberTail = value % step;
+    var numberTail = value % STEP;
 
     if (value === numberTail) {
-      if (value > step / 2) {
-        value = step;
+      if (value > STEP / 2) {
+        value = STEP;
       } else {
-        value = min;
+        value = MIN;
       }
       return value;
     }
@@ -54,8 +73,8 @@ window.roller = function (params) {
     if (value > numberTail) {
       value = value - numberTail;
 
-      if (value > max || value + numberTail >= max) {
-        value = max;
+      if (value > MAX || value + numberTail >= MAX) {
+        value = MAX;
       }
       return value;
     }
@@ -74,63 +93,56 @@ window.roller = function (params) {
   }
 
   var move = function (pointerPosition, target) {
-    var maxRight = rollerParent.offsetWidth;
-    var parentLongitude = rollerParent.getBoundingClientRect().left;
-    var rollerPosition = pointerPosition - parentLongitude;
+    var procentGap = getProcentGap();
+    var maxRight = rollerNode.offsetWidth;
+    var parentLongitudeFromLeft = rollerNode.getBoundingClientRect().left;
+    var rollerPosition = pointerPosition - parentLongitudeFromLeft;
     var percent = getHandlerPositionPercent(rollerPosition, maxRight);
 
-    if (!roller2Exist) {
+    if (!isSecondHandler) {
       // значение ширины оси
-      roller.style.left = percent + '%';
+      rollerHandler.style.left = percent + '%';
 
-      value1 = ((percent * differenceValue / 100) + min).toFixed(0);
+      value1 = ((percent * DIFFERENCE / 100) + MIN).toFixed(0);
       value1 = roundOff(value1);
 
-      if (shaftIndicator) {
-        shaftIndicator.style.width = 100 - percent + '%';
-      }
+      shaftIndicator.style.width = 100 - percent + '%';
       currentChangedInput = 1;
     }
     // end one roller condition
 
-    if (roller2Exist) {
-      roller1position = Number(roller.style.left.split('%')[0]) || roller1position;
-      roller2position = Number(roller2.style.right.split('%')[0]) || roller2position;
+    if (isSecondHandler) {
+      handlerPosition = Number(rollerHandler.style.left.split('%')[0]) || handlerPosition;
+      secondHandlerPosition = Number(rollerHandler2.style.right.split('%')[0]) || secondHandlerPosition;
 
-      if (target === roller) {
-        currentTarget = roller;
-      } else if (target === roller2) {
-        currentTarget = roller2;
+      if (target === rollerHandler || target === rollerHandler2) {
+        currentTarget = target;
       }
 
-      if (currentTarget === roller) {
+      if (currentTarget === rollerHandler) {
         currentChangedInput = 1;
 
-        if (percent + roller2position <= 100 - procentGap) {
-          roller.style.left = percent + '%';
-          value1 = ((percent * differenceValue / 100) + min).toFixed(0);
+        if (percent + secondHandlerPosition <= 100 - procentGap) {
+          rollerHandler.style.left = percent + '%';
+          value1 = ((percent * DIFFERENCE / 100) + MIN).toFixed(0);
           value1 = roundOff(value1);
 
-          if (shaftIndicator) {
-            shaftIndicatorFromLeft = percent;
-            shaftIndicator.style.width = 100 - (shaftIndicatorFromLeft + shaftIndicatorFromRight) + '%';
-          }
+          shaftIndicatorFromLeft = percent;
+          shaftIndicator.style.width = 100 - (shaftIndicatorFromLeft + shaftIndicatorFromRight) + '%';
         }
       }
 
-      if (currentTarget === roller2) {
+      if (currentTarget === rollerHandler2) {
         currentChangedInput = 2;
 
-        if ((100 - percent) + roller1position <= 100 - procentGap) {
-          roller2.style.right = (100 - percent) + '%';
-          value2 = ((percent * differenceValue / 100) + min).toFixed(0);
+        if ((100 - percent) + handlerPosition <= 100 - procentGap) {
+          rollerHandler2.style.right = (100 - percent) + '%';
+          value2 = ((percent * DIFFERENCE / 100) + MIN).toFixed(0);
           value2 = roundOff(value2);
 
-          if (shaftIndicator) {
-            shaftIndicatorFromRight = (100 - percent);
-            shaftIndicator.style.width = 100 - (shaftIndicatorFromLeft + shaftIndicatorFromRight) + '%';
-            shaftIndicator.style.right = (100 - percent) + '%';
-          }
+          shaftIndicatorFromRight = (100 - percent);
+          shaftIndicator.style.width = 100 - (shaftIndicatorFromLeft + shaftIndicatorFromRight) + '%';
+          shaftIndicator.style.right = (100 - percent) + '%';
         }
       }
     }
@@ -142,30 +154,30 @@ window.roller = function (params) {
   // end move function
 
   if (params.startValue) {
-    var startCoef = params.startValue > max ? 1 : (params.startValue - min) / differenceValue;
-    var startPx = maxRight * startCoef + parentLongitude;
+    var startCoef = params.startValue > MAX ? 1 : (params.startValue - MIN) / DIFFERENCE;
+    var startPx = maxRight * startCoef + parentLongitudeFromLeft;
     move(startPx);
   }
 
   if (params.endValue) {
-    var endCoef = params.endValue > max ? 1 : (params.endValue - min) / differenceValue;
-    var endPx = maxRight * endCoef + parentLongitude;
-    move(endPx, roller2);
+    var endCoef = params.endValue > MAX ? 1 : (params.endValue - MIN) / DIFFERENCE;
+    var endPx = maxRight * endCoef + parentLongitudeFromLeft;
+    move(endPx, rollerHandler2);
   }
 
   // событие по клику на ось
   var clickListener = function (e) {
-    if (roller2Exist) {
-      if (e.target !== roller && e.target !== roller2) {
+    if (isSecondHandler) {
+      if (e.target !== rollerHandler && e.target !== rollerHandler2) {
         move(e.pageX);
       }
       return;
     }
 
-    if (e.target !== roller) move(e.pageX);
+    if (e.target !== rollerHandler) move(e.pageX);
   }
 
-  rollerParent.addEventListener('click', clickListener);
+  rollerNode.addEventListener('click', clickListener);
 
   // события для мыши
   var mouseListener = function (element) {
@@ -183,10 +195,10 @@ window.roller = function (params) {
     };
   }
 
-  mouseListener(roller);
+  mouseListener(rollerHandler);
 
-  if (roller2Exist) {
-    mouseListener(roller2);
+  if (isSecondHandler) {
+    mouseListener(rollerHandler2);
   }
 
   // события для touch
@@ -204,9 +216,9 @@ window.roller = function (params) {
     }
   }
 
-  touchListener(roller);
+  touchListener(rollerHandler);
 
-  if (roller2Exist) {
-    touchListener(roller2);
+  if (isSecondHandler) {
+    touchListener(rollerHandler2);
   }
 };
