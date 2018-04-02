762e4f70964a7792f1546847ffbb36e48a28ca5c
Артём <hoodsey20@gmail.com>
2018-04-02 12:13:56 +0700

    сделал компонент табов доступным

--- src/components/tabs/scripts.js
+++ src/components/tabs/scripts.js
@@ -1,10 +1,22 @@
-var $ = require('jquery')
+var $ = require('jquery');
 
-$('body').on('click', '.js_tabs .tabs__item', function (e) {
-  var choosenIndex = $(this).attr('data-tabitem')
-  var tabParent = $(this).parents('.js_tabs')
-  tabParent.find('.tabs__item').removeClass('isActive')
-  $(this).addClass('isActive')
-  tabParent.find('.tabs__contentItem').removeClass('isOpen')
-  tabParent.find('.tabs__contentItem[data-tabcontent="' + choosenIndex + '"]').addClass('isOpen')
-})
+var tabsWork = function (e, jqSelf) {
+  var choosenIndex = jqSelf.attr('data-tabitem');
+  var tabParent = jqSelf.parents('.js-tabs');
+  tabParent.find('.tabs__item').removeClass('isActive');
+  jqSelf.addClass('isActive');
+  tabParent.find('.tabs__contentItem').removeClass('isOpen');
+  tabParent.find('.tabs__contentItem[data-tabcontent="' + choosenIndex + '"]').addClass('isOpen');
+}
+
+$('body').on('click', '.js-tabs .tabs__item', function (e) {
+  var self = $(this);
+  tabsWork(e, self);
+});
+
+$('body').on('keypress', '.js-tabs .tabs__item', function (e) {
+  if (e.which === 13) {
+    var self = $(this);
+    tabsWork(e, self);
+  }
+});
