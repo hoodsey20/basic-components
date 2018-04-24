430fe39d7e049b474334921000b1463bb4f6ab9c
Артём <hoodsey20@gmail.com>
2018-04-24 18:54:29 +0700

    правки компонента button

--- src/components/button/scripts.js
+++ src/components/button/scripts.js
@@ -1,8 +1,6 @@
 var $ = require('jquery');
 
-$('.js-collapseCard .collapseCard__header').click(function (e) {
+$('.js-button').click(function (e) {
   e.preventDefault();
-  var collapsedCard = $(this).parents('.collapseCard');
-  collapsedCard.toggleClass('collapseCard_isClosed');
-  collapsedCard.toggleClass('collapseCard_isOpen');
+  window.alert('button click');
 })
