b61d7f5824792daef74e7e58dff9d48d7041eec8
Артём <hoodsey20@gmail.com>
2018-05-23 17:32:08 +0700

    изменил логику классов схлопывающейся карточки

--- src/components/collapseCard/scripts.js
+++ src/components/collapseCard/scripts.js
@@ -3,6 +3,6 @@ var $ = require('jquery');
 $('.js-collapseCard .collapseCard__header').click(function (e) {
   e.preventDefault();
   var collapsedCard = $(this).parents('.collapseCard');
-  collapsedCard.toggleClass('collapseCard_isClosed');
-  collapsedCard.toggleClass('collapseCard_isOpen');
+  collapsedCard.toggleClass('isClosed');
+  collapsedCard.toggleClass('isOpen');
 })
